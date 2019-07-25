a6c2fcf7db0207622c4db8bf66a7689e689697f3
José Miguel Gamarro Tornay <jmgt93@gmail.com>
2019-07-25 14:30:11 +0200

    inicio de estandarizacion

--- TODSLibreria/FuzzySimplexService/FuzzyPrimalSimplexService.cs
+++ TODSLibreria/FuzzySimplexService/FuzzyPrimalSimplexService.cs
@@ -9,9 +9,25 @@ namespace TODSLibreria.FuzzySimplexService
 {
     public class FuzzyPrimalSimplexService
     {
-        public IEnumerable<VectorEquation> StandardizeConstraints(IEnumerable<Constraint> restriccions)
+        public IEnumerable<VectorEquation> StandardizeConstraints(IEnumerable<Constraint> constraintList)
         {
+            List<VectorEquation> standardConstraints = new List<VectorEquation>();
 
+            if(constraintList != null && constraintList.Count() > 0)
+            {
+                int indice = 1;
+                List<string> standardHeader = new List<string>();
+
+                standardHeader = constraintList.Select(x => x.NombresVariables).FirstOrDefault().ToList();
+
+                foreach(Constraint c in constraintList)
+                {
+                    standardConstraints.Add(new VectorEquation(string.Format("S{0}", indice.ToString()), standardHeader, c.CuerpoNum, c.TerminoIndependiente));
+                }
+
+            }
+
+            return standardConstraints;
         }
     }
 }
